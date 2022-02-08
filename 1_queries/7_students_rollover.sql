f3c76110dba8a3e717c6d26bd1a2a4c069f2cb6f
mrjohnming <john.ngai@hotmail.com>
2022-02-08 00:46:33 +0000

    Get all the rollover students

---
+++ 1_queries/7_students_rollover.sql
@@ -0,0 +1,9 @@
+-- 7_students_rollover.sql
+-- Get all the rollover students.
+SELECT students.name, cohorts.name,
+  cohorts.start_date as cohort_start_date,
+  students.start_date as student_start_date
+FROM students
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.start_date <> students.start_date
+ORDER BY cohort_start_date;
